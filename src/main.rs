2c9d10133c67dfee2d0f5adfcb4078721557cd0e
Daniel Atkinson <hourd.tasa@gmail.com>
2015-05-24 00:51:59 +0100

    added sample websockets to project

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 extern crate iron;
 extern crate router;
+extern crate websocket;
 
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+use websocket::{Server, Message};
+use std::thread;
 
 fn main() {
     let mut router = Router::new();
     router.get("/", handler);
     router.get("/:query", handler);
+
+
+    let server = Server::bind("127.0.0.1:3003").unwrap();
+
+
+    for connection in server {
+        // Spawn a new thread for each connection.
+        thread::spawn(move || {
+           let request = connection.unwrap().read_request().unwrap(); // Get the request
+           let response = request.accept(); // Form a response
+           let mut client = response.send().unwrap(); // Send the response
+
+           let message = Message::Text("Hello, client!".to_string());
+           let _ = client.send_message(message);
+        });
+    }
 
     Iron::new(router).http("0.0.0.0:3000").unwrap();
 }
